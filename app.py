ec28962db6d788f637d68a63658c9117ffc5a7da
dharan <153903199+Atchayasakthivel@users.noreply.github.com>
2024-02-23 12:45:31 +0530

    d

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask 
+from flask import Flask ,render_template
 
 app=Flask(__name__)
 
 
 @app.route('/index')
 def index():
-    return "welcome"
+    return render_template('index.html')
 
 
 if __name__ =="__main__":
